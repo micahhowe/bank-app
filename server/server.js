c69d05fe71b8a7213103d4a72cb270e7664acdaa
micahhowe <micahhowe182@gmail.com>
2019-08-08 10:35:12 -0600

    server is working now

---
+++ server/server.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+const express = require('express')
+const massive = require('massive')
+const session = require('express-session')
+const {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET} = process.env
+
+const app = express()
+
+app.use(express.json())
+
+massive(CONNECTION_STRING).then(db => {
+  app.set('db', db)
+  app.listen(SERVER_PORT, () => console.log(`Still Listening on ${SERVER_PORT}`))
+})
