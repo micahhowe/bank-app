49fbf4b879086b2ba5bc2c6729a8d688a29a826e
micahhowe <micahhowe182@gmail.com>
2019-08-08 11:16:42 -0600

    set up a few routes

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,11 +1,14 @@
 import React, {Component} from 'react'
 import './Dashboard.css'
+import Transaction from '../Transaction/Transaction'
+
 
 export default class Dashboard extends Component {
   render() {
     return (
       <div className="Dashboard">
         Dashboard
+        <Transaction />
       </div>
     )
   }
--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -2,6 +2,11 @@ import React, {Component} from 'react'
 import './Landing.css'
 
 export default class Landing extends Component {
+    state = {
+        username: '',
+        email: '',
+        password: ''
+      }
   render() {
     return (
       <div className="Landing">
