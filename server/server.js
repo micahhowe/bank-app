2f45d6c9b6d4df4edcc1a093b9a6aab02c2b0eba
micahhowe <micahhowe182@gmail.com>
2019-08-08 10:41:00 -0600

    initial server setup

--- server/server.js
+++ server/server.js
@@ -7,6 +7,14 @@ const {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET} = process.env
 const app = express()
 
 app.use(express.json())
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 10
+    }
+  }))
 
 massive(CONNECTION_STRING).then(db => {
   app.set('db', db)
